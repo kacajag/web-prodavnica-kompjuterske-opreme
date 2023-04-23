92a01b226b76c6076670d1673548c69b2260a80b
Miljana00 <teofilovicmiljana00@gmail.com>
2023-04-23 21:28:53 +0200

    dodata komponenta footer

---
+++ resources/js/components/Footer.jsx
@@ -0,0 +1,31 @@
+ 
+
+function Footer() {
+    return (
+      <div  >
+              
+    <footer className="site-footer">
+        
+        <div className="container">
+            <div className="row">
+            <div className="col-md-8 col-sm-6 col-xs-12">
+                <p className="copyright-text">Copyright &copy; 2022 All Rights Reserved by KNM
+            
+                </p>
+            </div>
+
+            
+            </div>
+        </div>
+    </footer>
+      </div>
+  
+  
+  
+  
+  
+    );
+  }
+  
+  export default Footer;
+  
