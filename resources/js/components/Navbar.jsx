7e0fdfd3a5bcf542035ff47e735c2ea9a06a30d2
nevena-dav <nevena.davidovic.19@gmail.com>
2023-04-23 21:26:23 +0200

    dodata komponenta navbar

---
+++ resources/js/components/Navbar.jsx
@@ -0,0 +1,21 @@
+ 
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div className="topnav">
+        <Link to="/">Pocetna</Link>
+        <Link to="/kontakt">Kontakt</Link>
+     
+        
+
+    </div>
+
+
+
+
+
+  );
+}
+
+export default Navbar;
