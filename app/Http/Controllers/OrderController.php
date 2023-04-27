6de1fa3a1e70e4cf1e398ee52a16858211ee8ea3
nevena-dav <nevena.davidovic.19@gmail.com>
2023-04-27 22:19:10 +0200

    omogucena obrada porudzbine, porudzbina i stavke se cuvaju u bazi

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -37,31 +38,41 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        
         $validator = Validator::make(
             $request->all(),
             [
-                'user_id' =>  'required' , 
-              
-                'total_price' => ' ',
-                'order_date' => 'required'  ,
-                
+                'user_id' => 'required',
+                'total_price' => '',
+                'order_date' => 'required',
+                'order_items' => 'required|array|min:1',
+                'order_items.*.product_id' => 'required|integer',
+                'order_items.*.quantity' => 'required|integer|min:1',
+                'order_items.*.price' => 'required|numeric|min:0'
             ]
         );
-        if ($validator->fails()) 
+    
+        if ($validator->fails()) {
             return response()->json($validator->errors());
- 
-
-        $o = Order::create([
-                'user_id' =>   $request->user_id, 
-                'total_price' => $request->total_price, 
-                'order_date' =>  $request->order_date,                
-            
-                 
-           
-        ]);
-        return response()->json(["Uspesno kreirana stavka!",$o]);
+        }
+        $orderDate = date('Y-m-d H:i:s', strtotime($request->order_date));
+        $order = new Order();
+        $order->user_id = $request->user_id;
+        $order->total_price = $request->total_price;
+        $order->order_date = $orderDate;
+        $order->save();
+    
+        $order_items = $request->order_items;
+        foreach ($order_items as $order_item) {
+            $order_item_model = new OrderItem();
+            $order_item_model->order_id = $order->id;
+            $order_item_model->product_id = $order_item['product_id'];
+            $order_item_model->quantity = $order_item['quantity'];
+            $order_item_model->save();
+        }
+    
+        return response()->json(["Uspesno!",new OrderResource(  $order)]);
     }
+    
 
     /**
      * Display the specified resource.
--- resources/js/components/Cart.jsx
+++ resources/js/components/Cart.jsx
@@ -4,8 +4,56 @@ import { Modal } from 'react-bootstrap';
 import CartItem from './CardItem';
 
 const Cart = ({ cartItems, removeItem,show,setShow }) => {
- 
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
   
+    const handleOrder = async () => {
+        try {
+          setIsLoading(true);
+          setError('');
+          setSuccess('');
+    
+          const orderItems = cartItems.map((item) => ({
+            product_id: item.id,
+            quantity: 1,
+            price: item.price,
+          }));
+    
+          const orderData = {
+            user_id: window.sessionStorage.getItem("auth_id"), 
+            order_items: orderItems,
+            order_date: new Date(),
+            total_price: countTotal()
+
+          };
+    
+          const response = await fetch('/api/orders', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(orderData),
+          });
+    
+          const data = await response.json();
+          if (response.ok) {
+            setSuccess('Uspešno ste kreirali porudžbinu.');
+            removeItem(cartItems); // brisanje stavki iz korpe nakon kreiranja porudžbine
+          } else {
+            setError(data.message || 'Došlo je do greške prilikom kreiranja porudžbine.');
+          }
+        } catch (error) {
+          console.error(error);
+          setError('Došlo je do greške prilikom komunikacije sa serverom.');
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+
+
+
     const handleClose = () => setShow(false);
  
   
@@ -22,27 +70,29 @@ const Cart = ({ cartItems, removeItem,show,setShow }) => {
     return (
       <>
  
-  
-            <Modal show={show} onHide={() => handleClose()}>
-            <Modal.Header closeButton>
-                <Modal.Title>Shopping Cart</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {cartItems && cartItems.length > 0 ? (
-                <div>
-                    {cartItems.map((item) => (
-                    <CartItem key={item.id} item={item} removeItem={removeItem} />
-                    ))}
-                    <p>Total: {countTotal()} RSD</p>
-                </div>
-                ) : (
-                <p>Your cart is empty</p>
-                )}
-            </Modal.Body>
-            <Modal.Footer>
-                <button onClick={handleClose}>Close</button>
-            </Modal.Footer>
-            </Modal>
+            
+            <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Shopping Cart</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    {cartItems && cartItems.length > 0 ? (
+                        <div>
+                        {cartItems.map((item) => (
+                            <CartItem key={item.id} item={item} removeItem={removeItem} />
+                        ))}
+                        <p>Total: {countTotal()} RSD</p>
+                        <button disabled={isLoading} onClick={handleOrder}>
+                            Kreiraj porudžbinu
+                        </button>
+                        {error && <div className="error">{error}</div>}
+                        {success && <div className="success">{success}</div>}
+                        </div>
+                    ) : (
+                        <p>Your cart is empty</p>
+                    )}
+                    </Modal.Body>
+                </Modal>
 
       </>
     );
