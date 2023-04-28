ed84ba12cd4747c5211398d58a7daf500a92f3da
Miljana00 <teofilovicmiljana00@gmail.com>
2023-04-28 17:43:02 +0200

    zavrsena update funkcija

--- resources/js/components/ProductList.jsx
+++ resources/js/components/ProductList.jsx
@@ -1,19 +1,45 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Table } from 'react-bootstrap';
+import axios from 'axios'; 
 import { MDBDataTableV5 } from 'mdbreact';
-const ProductList = ({products,setProducts}) => {
- 
+import EditProductModal from './EditProductModal';
 
-  const handleDelete = async (productId) => {
-    try {
-      const response = await axios.delete(`/api/products/${productId}`);
-      console.log(response.data);
-      setProducts(products.filter(product => product.id !== productId));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+const ProductList = ({products,setProducts}) => {
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [editedProduct, setEditedProduct] = useState(null);
+    
+    const handleEdit = (productId) => {
+        const productToEdit = products.find((product) => product.id === productId);
+        if (productToEdit) {
+          setShowEditModal(true);
+          setEditedProduct(productToEdit);
+        }
+      };
+      
+ 
+  
+    const handleDelete = async (productId) => {
+      try {
+        const response = await axios.delete(`/api/products/${productId}`);
+        console.log(response.data);
+        setProducts(products.filter(product => product.id !== productId));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+ 
+    const handleUpdate = async (productId, updatedData) => {
+        try {
+          const response = await axios.put(`/api/products/${productId}`, updatedData);
+          console.log(response.data);
+          setProducts(
+            products.map((product) => (product.id === productId ? { ...product, ...updatedData } : product))
+          );
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      
+  
   const datatable = {
     columns: [
       {
@@ -55,13 +81,24 @@ const ProductList = ({products,setProducts}) => {
       category_name: product.category.name,
       options: (
         <>
-          <button className="btn btn-warning">Izmeni</button>
+          <button className="btn btn-warning"  onClick={() => handleEdit(product.id)}>Izmeni</button>
           <button className="btn btn-danger" onClick={() => handleDelete(product.id)}>Obriši</button>
         </>
       )
     }))
   };
-  return <div className='tabelaProducts'><MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={10} pagesAmount={4} data={datatable} /></div>;
+  return <div className='tabelaProducts'>
+    <MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={10} pagesAmount={4} data={datatable} />
+        {editedProduct && (
+        <EditProductModal
+            show={showEditModal}
+            handleClose={() => setShowEditModal(false)}
+            product={editedProduct}
+            handleUpdate={handleUpdate}
+        />
+        )}
+     
+    </div>;
 
 };
 
